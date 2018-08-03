6c2db2b84f5a2f9123ccfb010be60c2e1191f0d7
oliver-contier <oliver.contier@ovgu.de>
2018-08-03 10:29:00 +0200

    small cosmetic chnges for presentation at bayes symposium

--- test_brms.R
+++ test_brms.R
@@ -8,19 +8,20 @@ load("~/Desktop/hmax/python/compare_images/prepped_data.RData", ex <- new.env())
 # Load test data from environment
 disc_df_brms <- ex$disc_df_brms
 str(disc_df_brms)
+head(disc_df_brms)
 
 # try this
 disc_df_brms$Block <- factor(disc_df_brms$Block, ordered = FALSE)
 
 # set up maximally complex model
 discmodel1 <- brm(Answer ~ Block*ED + (Block*ED|sub) + (1|item_id),
-             data = disc_df_brms,
-             family = bernoulli,  #bernoulli
-             chains = 2, cores = 2)
+              data = disc_df_brms,
+              family = bernoulli,  #bernoulli
+              chains = 2, cores = 2)
 
 #save.image(file = "disc_maxmodel.RData", version = NULL, safe = TRUE)
-#load("~/Desktop/hmax/python/compare_images/disc_maxmodel.RData", ex <- new.env())
-#discmodel1 <- ex$discmodel1
+load("~/Desktop/hmax/python/compare_images/disc_maxmodel.RData", ex <- new.env())
+discmodel1 <- ex$discmodel1
 
 summary(discmodel1)
 
@@ -31,15 +32,15 @@ summary(discmodel1)
 plot(discmodel1, ask = FALSE)
 pp_check(discmodel1)  # check posterior predictions
 plot(marginal_effects(discmodel1), ask = FALSE)  # plot marginal predictions
+plot(discmodel1, pars="^sd")
 
-# TODO: Interpraetierarkeit des outputs verbessern
-# kontinuierliche praediktoren zentrieren
-# Effektkodierung für kategorielle Prädiktoren
+# TODO: interpretability
+# Centralize continuous predictors (ED)
+# Effect coding for categorical predictors (Block)
 
 # TODO: hypothesis testing
-# hypothesis function to assess contrasts
-# reduce model complexity and compare with LOO
+# Assess contrasts
+# Reduce model complexity and compare with LOO
 
 # TODO: outlier weighting?
-# There's no way to model the influence of outliers in the predictors (only the response)
-# Hence, compare models based on data including and excluding outliers.
+# Compare models based on data including and excluding outliers.
