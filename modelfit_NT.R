3826d0a94592e5cebb8083b74fa8181fbc595b31
oliver-contier <oliver.contier@ovgu.de>
2018-08-27 10:58:06 +0200

    tested bunch of different null models. next: change rfx structure of models of interest according to best fitting null model.

--- modelfit_NT.R
+++ modelfit_NT.R
@@ -10,13 +10,38 @@ df <- ex$df_total
 head(df)
 str(df)
 
-print('fitting nullmodel')
+print('fitting nullmodel1')
 # Null model
 answer_nullmodel <- brm(Answer ~ Block + (Block|sub) + (1|item_id) + (1|task),
                         data = df,
                         family = bernoulli,
                         file = "answer_nullmodel",
                         chains = 2, cores = 2)
+
+print('fitting nullmodel2')
+# Null model with varying slope for Block over tasks and over subjects
+answer_nullmodel2 <- brm(Answer ~ Block + (Block|sub) + (Block|task) + (1|item_id),
+                        data = df,
+                        family = bernoulli,
+                        file = "answer_nullmodel2",
+                        chains = 2, cores = 2)
+
+print('fitting nullmodel3')
+# Null model with random slope for block and task as well as their interaction across subjects
+answer_nullmodel3 <- brm(Answer ~ Block*task + (Block*task|sub) + (1|item_id),
+                        data = df,
+                        family = bernoulli,
+                        file = "answer_nullmodel3",
+                        chains = 2, cores = 2)
+
+print('fitting nullmodel4')
+# Null model equivalent to nullmodel3, but with random item slope over tasks.
+answer_nullmodel4 <- brm(Answer ~ Block*task + (Block*task|sub) + (item_id|task),
+                        data = df,
+                        family = bernoulli,
+                        file = "answer_nullmodel4",
+                        chains = 2, cores = 2)
+
 
 print('fitting newNT model')
 # newfound typicality
