ddab67635f306d62aea262bda45fdcf703148d91
oliver-contier <oliver.contier@ovgu.de>
2018-09-19 13:25:41 +0200

    divergent transitions for NT models. This is getting out of hand. Let's try it the simple way with lme4

--- modelfit_NT.R
+++ modelfit_NT.R
@@ -15,6 +15,9 @@ df <- ex$df_total
 head(df)
 str(df)
 
+
+# Null models
+
 print('fitting nullmodel1')
 answer_nullmodel <- brm(Answer ~ ec_b2 + ec_b3 + task + task_order + (ec_b2 + ec_b3 + task + task_order|sub) + (1|item_id),
                         data=df,
@@ -57,38 +60,59 @@ answer_nullmodel5 <- brm(Answer ~ ec_b2 + ec_b3 + task + task_order + task:task_
                         chains=2, cores=2,
                         inits=0)
 
-print('comparing null models with LOO')
-loo_nullmodcomp_nills <- loo(answer_nullmodel, answer_nullmodel2, answer_nullmodel3, answer_nullmodel4, answer_nullmodel5)
-print(loo_nullmodcomp_nulls)
+print('fitting nullmodel6')
+answer_nullmodel6 <- brm(Answer ~ ec_b2 + ec_b3 + task + task_order + task:task_order + (ec_b2 + ec_b3 + task + task_order|sub) + (1|item_id),
+                        data=df,
+                        family=bernoulli,
+                        file="answer_nullmodel6",
+                        sample_prior=TRUE, save_all_pars=TRUE,
+                        chains=2, cores=2,
+                        inits=0)
 
-# print('fitting newNT model')
-# # newfound typicality
-# answer_newNT <- brm(Answer ~ newNT_cent*Block*task + (newNT_cent*Block*task|sub) + (1|item_id),
-#                     data=df,
-#                     #priors=set_prior("normal(0,100)", class="b")
-#                     family=bernoulli,
-#                     file="answer_newNT",
-#                     save_all_pars=TRUE,
-#                     chains=2, cores=2)
-#
-# print('fitting conNT model')
-# # conserved typicality
-# answer_conNT <- brm(Answer ~ conNT_cent*Block*task + (conNT_cent*Block*task|sub) + (1|item_id),
-#                     data=df,
-#                     family=bernoulli,
-#                     #priors=set_prior("normal(0,100)", class="b")
-#                     file="answer_conNT",
-#                     save_all_pars=TRUE,
-#                     chains=2, cores=2)
-#
-# print('fitting bothNT model')
-# # newfound and conserved typicality
-# answer_bothNT <- brm(Answer ~ newNT_cent*conNT_cent*Block*task + (newNT_cent*conNT_cent*Block*task|sub) + (1|item_id),
-#                     data=df,
-#                     family=bernoulli,
-#                     #priors=set_prior("normal(0,100)", class="b")
-#                     file="answer_bothNT",
-#                     save_all_pars=TRUE,
-#                     chains=2, cores=2)
+print('fitting nullmodel7')
+answer_nullmodel7 <- brm(Answer ~ ec_b2 + ec_b3 + task + task_order + task:task_order + ec_b2:task + ec_b3:task + ec_b2:task:task_order + ec_b3:task:task_order + (ec_b2 + ec_b3 + task + task_order|sub) + (1|item_id),
+                        data=df,
+                        family=bernoulli,
+                        file="answer_nullmodel7",
+                        sample_prior=TRUE, save_all_pars=TRUE,
+                        chains=2, cores=2,
+                        inits=0)
+
+
+# Neural Typicality models
+
+print('fitting bothNT model')
+answer_bothNT <- brm(Answer ~ ec_b2 + ec_b3 + task + task_order + conNT + newNT + conNT:newNT + conNT:ec_b2 + conNT:ec_b3 + newNT:ec_b2 + newNT:ec_b3 + conNT:task + newNT:task + task:ec_b2 + task:ec_b3 + (ec_b2 + ec_b3 + task + task_order + conNT + newNT|sub) + (1|item_id),
+                        data=df,
+                        family=bernoulli,
+                        file="answer_bothNT",
+                        sample_prior=TRUE, save_all_pars=TRUE,
+                        chains=2, cores=2)
+
+print('fitting newNT model')
+answer_newNT <- brm(Answer ~ ec_b2 + ec_b3 + task + task_order + newNT + newNT:ec_b2 + newNT:ec_b3 + newNT:task + task:ec_b2 + task:ec_b3 + (ec_b2 + ec_b3 + task + task_order + newNT|sub) + (1|item_id),
+                    data=df,
+                    family=bernoulli,
+                    file="answer_newNT",
+                    sample_prior=TRUE, save_all_pars=TRUE,
+                    chains=2, cores=2)
+
+print('fitting conNT model')
+answer_conNT <- brm(Answer ~ ec_b2 + ec_b3 + task + task_order + conNT + conNT:ec_b2 + conNT:ec_b3 + conNT:task + task:ec_b2 + task:ec_b3 + (ec_b2 + ec_b3 + task + task_order + conNT|sub) + (1|item_id),
+                    data=df,
+                    family=bernoulli,
+                    file="answer_conNT",
+                    sample_prior=TRUE, save_all_pars=TRUE,
+                    chains=2, cores=2)
+
+print('comparing models with LOO')
+loo_modcomp <- loo(answer_nullmodel, answer_nullmodel2, answer_nullmodel3,
+  answer_nullmodel4, answer_nullmodel5, answer_nullmodel6, answer_nullmodel7,
+  answer_bothNT, answer_newNT, answer_conNT)
+print(loo_modcomp)
+
+# Save LOO results to rds file
+print('saving LOO results')
+saveRDS(loo_modcomp, file = "loo_modcomp.rds")
 
 print('DONE')
