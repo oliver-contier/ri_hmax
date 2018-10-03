ac6de6a330c9c3a3cc71bbfd1df6acaac95bd3bc
oliver-contier <oliver.contier@ovgu.de>
2018-10-03 15:48:55 +0200

    finished first round of modelling with lmerTest. Results: RT can be significantly explained by conNT and newNT measures.

--- lme4_modelfit.R
+++ lme4_modelfit.R
@@ -1,5 +1,3 @@
-# execute GLM analyses with lme4 on our cluster
-
 print('load packages')
 #library(lme4)
 library(lmerTest)
@@ -13,130 +11,246 @@ df <- ex$df_total
 head(df)
 str(df)
 
+# #TODO: Create function and loop
+# # specify ffx and rfx as vectors
+# ffx <- paste("ED", "Block", "", sep="+")
+# rfx_sub <- paste("ED", "Block", sep="+")
+# # build a formula out of these vectors
+# # and plug formla into glmer
+# form <- paste("Answer~", ffx, "(", rfx, "|sub) + (1|item_id)", sep="")
+# test <- glmer(form, data = df, family = binomial)
 
 # Accuracies (logistic models)
 print('fitting accuracies')
 
-## ED and CD models
-answer_bothD <- glmer(Answer ~ ED + CD + Block + task + task_order +
-                      (ED + CD + Block + task + task_order|sub) + (1|item_id),
-                      data = df, family = binomial,
-                      control=glmerControl(optimizer ="Nelder_Mead"))
+  ## ED and CD models
+rdspath <- "./lme4_data/answer_bothD.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  answer_bothD <- glmer(Answer ~ ED + CD + Block + task + task_order +
+                          (ED + CD + Block + task + task_order|sub) + (1|item_id),
+                          data = df, family = binomial)
+  saveRDS(answer_bothD, rdspath)
+}
 
-answer_ED <- glmer(Answer ~ ED + Block + task + task_order +
-                      (ED + Block + task + task_order|sub) + (1|item_id),
-                      data = df, family = binomial,
-                      control=glmerControl(optimizer ="Nelder_Mead"))
-
-answer_CD <- glmer(Answer ~ CD + Block + task + task_order +
-                      (CD + Block + task + task_order|sub) + (1|item_id),
-                      data = df, family = binomial,
-                      control=glmerControl(optimizer ="Nelder_Mead"))
-
-## conNT and newNT models
-answer_bothNT <- glmer(Answer ~ conNT_z + newNT_z + Block + task + task_order +
-                      (conNT_z + newNT_z + Block + task + task_order|sub) + (1|item_id),
-                      data = df, family = binomial,
-                      control=glmerControl(optimizer ="Nelder_Mead"))
-
-# simpler version of bothNT model (no RFX for NT measures)
-answer_bothNT2 <- glmer(Answer ~ conNT_z + newNT_z + Block + task + task_order +
-                      (Block + task + task_order|sub) + (1|item_id),
-                      data = df, family = binomial,
-                      control=glmerControl(optimizer ="Nelder_Mead"))
-
-answer_conNT <- glmer(Answer ~ conNT_z + Block + task + task_order +
-                      (conNT_z + Block + task + task_order|sub) + (1|item_id),
-                      data = df, family = binomial,
-                      control=glmerControl(optimizer ="Nelder_Mead"))
-
-answer_newNT <- glmer(Answer ~ newNT_z + Block + task + task_order +
-                      (newNT_z + Block + task + task_order|sub) + (1|item_id),
-                      data = df, family = binomial,
-                      control=glmerControl(optimizer ="Nelder_Mead"))
-
-## null model
-answer_null <- glmer(Answer ~ Block + task + task_order +
-                      (Block + task + task_order|sub) + (1|item_id),
-                      data = df, family = binomial,
-                      control=glmerControl(optimizer ="Nelder_Mead"))
-
-
-# Reaction times (linear model)
-# Note that ML instead of REML must be used to allow comparison of models
-# with different fixed effects
-print('fitting RT')
-
-## ED and CD models
-rt_bothD <- lmer(RT ~ ED + CD + Block + task + task_order +
-                  (ED + CD + Block + task + task_order|sub) + (1|item_id),
-                  data = df, control = lmerControl(optimizer ="Nelder_Mead"), REML=F)
-
-rt_ED <- lmer(RT ~ ED + Block + task + task_order +
+rdspath <- "./lme4_data/answer_ED.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  answer_ED <- glmer(Answer ~ ED + Block + task + task_order +
                   (ED + Block + task + task_order|sub) + (1|item_id),
-                  data = df, control = lmerControl(optimizer ="Nelder_Mead"), REML=F)
+                  data = df, family = binomial)
+  saveRDS(answer_ED, rdspath)
+}
 
-rt_CD <- lmer(RT ~ CD + Block + task + task_order +
-                  (CD + Block + task + task_order|sub) + (1|item_id),
-                  data = df, control = lmerControl(optimizer ="Nelder_Mead"), REML=F)
+rdspath <- "./lme4_data/answer_CD.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  answer_CD <- glmer(Answer ~ CD + Block + task + task_order +
+                        (CD + Block + task + task_order|sub) + (1|item_id),
+                        data = df, family = binomial)
+  saveRDS(answer_CD, rdspath)
+}
+
+  ## conNT and newNT models
+rdspath <- "./lme4_data/answer_bothNT.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  answer_bothNT <- glmer(Answer ~ conNT_z + newNT_z + Block + task + task_order +
+                        (conNT_z + newNT_z + Block + task + task_order|sub) + (1|item_id),
+                        data = df, family = binomial)
+  saveRDS(answer_bothNT, rdspath)
+}
+
+rdspath <- "./lme4_data/answer_bothNT2.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  # simpler version of bothNT model (no RFX for NT measures)
+  answer_bothNT2 <- glmer(Answer ~ conNT_z + newNT_z + Block + task + task_order +
+                        (Block + task + task_order|sub) + (1|item_id),
+                        data = df, family = binomial)
+  saveRDS(answer_bothNT2, rdspath)
+}
+
+rdspath <- "./lme4_data/answer_conNT.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  answer_conNT <- glmer(Answer ~ conNT_z + Block + task + task_order +
+                        (conNT_z + Block + task + task_order|sub) + (1|item_id),
+                        data = df, family = binomial)
+  saveRDS(answer_conNT, rdspath)
+}
+
+rdspath <- "./lme4_data/answer_newNT.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  answer_newNT <- glmer(Answer ~ newNT_z + Block + task + task_order +
+                        (newNT_z + Block + task + task_order|sub) + (1|item_id),
+                        data = df, family = binomial)
+  saveRDS(answer_newNT, rdspath)
+}
+
+  ## null model
+rdspath <- "./lme4_data/answer_null.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  answer_null <- glmer(Answer ~ Block + task + task_order +
+                        (Block + task + task_order|sub) + (1|item_id),
+                        data = df, family = binomial)
+  saveRDS(answer_null, rdspath)
+}
+
+
+# Reaction time models
+
+## ED and CD models
+rdspath <- "./lme4_data/rt_bothD.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_bothD <- lmer(RT ~ ED + CD + Block + task + task_order +
+                    (ED + CD + Block + task + task_order|sub) + (1|item_id),
+                    data = df, REML=F)
+  saveRDS(rt_bothD, rdspath)
+}
+
+rdspath <- "./lme4_data/rt_ED.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_ED <- lmer(RT ~ ED + Block + task + task_order +
+                    (ED + Block + task + task_order|sub) + (1|item_id),
+                    data = df, REML=F)
+  saveRDS(rt_ED, rdspath)
+}
+
+rdspath <- "./lme4_data/rt_CD.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_CD <- lmer(RT ~ CD + Block + task + task_order +
+                (CD + Block + task + task_order|sub) + (1|item_id),
+                data = df, REML=F)
+  saveRDS(rt_CD, rdspath)
+}
 
 ## conNT and newNT models
-rt_bothNT <- lmer(RT ~ conNT_z + newNT_z + Block + task + task_order +
+rdspath <- "./lme4_data/rt_bothNT.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_bothNT <- lmer(RT ~ conNT_z + newNT_z + Block + task + task_order +
                   (conNT_z + newNT_z + Block + task + task_order|sub) + (1|item_id),
-                  data = df, control = lmerControl(optimizer ="Nelder_Mead"), REML=F)
+                  data = df, REML=F)
+  saveRDS(rt_bothNT, rdspath)
+}
 
-# simpler version of bothNT model (no RFX for NT measures)
-rt_bothNT2 <- lmer(RT ~ conNT_z + newNT_z + Block + task + task_order +
-                  (Block + task + task_order|sub) + (1|item_id),
-                  data = df, control = lmerControl(optimizer ="Nelder_Mead"), REML=F)
+# run lme4's RANOVA on our maximal bothNT model
+# for indication which RFX can be dropped
+rdspath <- "./lme4_data/rt_bothNT_ranova.rds"
+if (!file.exists(rdspath)){
+  rt_bothNT <- readRDS("./lme4_data/rt_bothNT.rds")
+  rt_bothNT_ranova <- ranova(rt_bothNT)
+  saveRDS(rt_bothNT_ranova, rdspath)
+}
 
-rt_conNT <- lmer(RT ~ conNT_z + Block + task + task_order +
+# reduce RFX of bothNT model
+rdspath <- "./lme4_data/rt_bothNT2.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_bothNT2 <- lmer(RT ~ conNT_z + newNT_z + Block + task + task_order +
+                    (Block + task + task_order|sub) + (1|item_id),
+                    data = df, REML=F)
+  saveRDS(rt_bothNT2, rdspath)
+}
+
+rdspath <- "./lme4_data/rt_bothNT3.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_bothNT3 <- lmer(RT ~ conNT_z + newNT_z + Block + task + task_order +
+                    (task + task_order|sub) + (1|item_id),
+                    data = df, REML=F)
+  saveRDS(rt_bothNT3, rdspath)
+}
+
+rdspath <- "./lme4_data/rt_bothNT4.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_bothNT4 <- lmer(RT ~ conNT_z + newNT_z + Block + task + task_order +
+                    (Block + task|sub) + (1|item_id),
+                    data = df, REML=F)
+  saveRDS(rt_bothNT4, rdspath)
+}
+
+rdspath <- "./lme4_data/rt_bothNT5.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_bothNT5 <- lmer(RT ~ conNT_z + newNT_z + Block + task + task_order +
+                    (Block + task_order|sub) + (1|item_id),
+                    data = df, REML=F)
+  saveRDS(rt_bothNT5, rdspath)
+}
+
+rdspath <- "./lme4_data/rt_bothNT6.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_bothNT6 <- lmer(RT ~ conNT_z + newNT_z + Block + task + task_order +
+                    (task|sub) + (1|item_id),
+                    data = df, REML=F)
+  saveRDS(rt_bothNT6, rdspath)
+}
+
+rdspath <- "./lme4_data/rt_bothNT7.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_bothNT7 <- lmer(RT ~ conNT_z + newNT_z + Block + task + task_order +
+                    (Block|sub) + (1|item_id),
+                    data = df, REML=F)
+  saveRDS(rt_bothNT7, rdspath)
+}
+
+# same as rt_bothNT4, but removed task_order as fixed-effect.
+rdspath <- "./lme4_data/rt_bothNT8.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_bothNT8 <- lmer(RT ~ conNT_z + newNT_z + Block + task +
+                    (Block + task|sub) + (1|item_id),
+                    data = df, REML=F)
+  saveRDS(rt_bothNT8, rdspath)
+}
+
+# ranova for bothNT model 8
+rdspath <- "./lme4_data/rt_bothNT8_ranova.rds"
+if (!file.exists(rdspath)){
+  rt_bothNT8 <- readRDS("./lme4_data/rt_bothNT8.rds")
+  rt_bothNT8_ranova <- ranova(rt_bothNT8)
+  saveRDS(rt_bothNT8_ranova, rdspath)
+}
+
+rdspath <- "./lme4_data/rt_conNT.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_conNT <- lmer(RT ~ conNT_z + Block + task + task_order +
                   (conNT_z + Block + task + task_order|sub) + (1|item_id),
-                  data = df, control = lmerControl(optimizer ="Nelder_Mead"), REML=F)
+                  data = df, REML=F)
+  saveRDS(rt_conNT, rdspath)
+}
 
-rt_newNT <- lmer(RT ~ newNT_z + Block + task + task_order +
+rdspath <- "./lme4_data/rt_newNT.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_newNT <- lmer(RT ~ newNT_z + Block + task + task_order +
                   (newNT_z + Block + task + task_order|sub) + (1|item_id),
-                  data = df, control = lmerControl(optimizer ="Nelder_Mead"), REML=F)
+                  data = df, REML=F)
+  saveRDS(rt_newNT, rdspath)
+}
 
-## null model
-rt_null <- lmer(RT ~ conNT_z + Block + task + task_order +
-                (conNT_z + Block + task + task_order|sub) + (1|item_id),
-                data = df, control = lmerControl(optimizer ="Nelder_Mead"), REML=F)
+rdspath <- "./lme4_data/rt_null.rds"
+paste("fitting", rdspath, sep=" ")
+if (!file.exists(rdspath)){
+  rt_null <- lmer(RT ~ Block + task + task_order +
+                  (Block + task + task_order|sub) + (1|item_id),
+                  data = df, REML=F)
+  saveRDS(rt_null, rdspath)
+}
 
-
-# Model comparison (likelihood ratio test with Rs ANOVA command)
-
-# # accuracies
-# print('comparing accuracies')
-# ## NT
-# answer_nullboth <- anova(answer_null, answer_bothNT)
-# answer_nullnew <- anova(answer_null, answer_newNT)
-# answer_nullcon <- anova(answer_null, answer_conNT)
-# answer_bothcon <- anova(answer_bothNT, answer_conNT)
-# answer_bothnew <- anova(answer_bothNT, answer_newNT)
-# ## CD/ED
-# answer_nullbothD <- anova(answer_null, answer_bothD)
-# answer_nullCD <- anova(answer_null, answer_CD)
-# answer_nullED <- anova(answer_null, answer_ED)
-# answer_bothCD <- anova(answer_bothD, answer_CD)
-# answer_bothED <- anova(answer_bothD, answer_ED)
-#
-# # rt
-# print('comparing RT')
-# ## NT
-# rt_nullboth <- anova(rt_null, rt_bothNT)
-# rt_nullnew <- anova(rt_null, rt_newNT)
-# rt_nullcon <- anova(rt_null, rt_conNT)
-# rt_bothcon <- anova(rt_bothNT, rt_conNT)
-# rt_bothnew <- anova(rt_bothNT, rt_newNT)
-# ## CD/ED
-# rt_nullbothD <- anova(rt_null, rt_bothD)
-# rt_nullCD <- anova(rt_null, rt_CD)
-# rt_nullED <- anova(rt_null, rt_ED)
-# rt_bothCD <- anova(rt_bothD, rt_CD)
-# rt_bothED <- anova(rt_bothD, rt_ED)
-
-# save data
-save.image(file = "lme4_modelfit.RData", version = NULL, safe = TRUE)
 
 print('DONE')
+
+# read rds
+#mod2 <- readRDS("mymodel.rds")
