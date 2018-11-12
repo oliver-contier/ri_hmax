d0284ff744a44ba2facb040832a95a2cd2f72476
oliver-contier <oliver.contier@ovgu.de>
2018-11-12 16:26:43 +0100

    finally figured out writing logg files correctly.

--- experiments/RI_objects_RSA/fmri_experiment/1back_task.py
+++ experiments/RI_objects_RSA/fmri_experiment/1back_task.py
@@ -6,9 +6,11 @@ hand-coded script for stimulus presentation in the scanner.
 Include both first and second session and make adaptable by input GUI.
 """
 
+import csv
 import glob
 import os
 import random
+from collections import OrderedDict
 from os.path import join as pjoin
 
 from psychopy import gui, core, data, visual, event
@@ -54,6 +56,7 @@ def create_gui(out_dir='./data',
         core.quit()
 
     # add additional info to exp_info which doesn't come from GUI input
+    exp_info = OrderedDict(exp_info)
     exp_info['exp_name'] = exp_name
     exp_info['date'] = data.getDateStr()
 
@@ -128,27 +131,26 @@ def get_stims_aloi_selection(percepts_dir='../Stimuli/percepts',
     for percept_fpath in percept_fpaths:
         # vision1 and vision2 are not actually used for the dict
         object_ID, rotation, vision1, vision2 = tuple(percept_fpath.split('/')[-1].split('.')[0].split('_'))
-        percept_dict = {'file_path': percept_fpath,
-                        'object_ID': object_ID,
-                        'rotation': int(rotation.replace('r', '')),
-                        'vision': 'ri_percept'}
+        percept_dict = OrderedDict({'file_path': percept_fpath,
+                                    'object_ID': object_ID,
+                                    'rotation': int(rotation.replace('r', '')),
+                                    'vision': 'ri_percept'})
         percept_dicts.append(percept_dict)
 
     # collect info from intact (prepped) object file names
     intact_dicts = []
     for intact_fpath in intact_fpaths:
         object_ID, rotation, vision = tuple(intact_fpath.split('/')[-1].split('.')[0].split('_'))
-        intact_dict = {'file_path': intact_fpaths,
-                       'object_ID': object_ID,
-                       'rotation': int(rotation.replace('r', '')),
-                       'vision': 'intact'}
+        intact_dict = OrderedDict({'file_path': intact_fpath,
+                                   'object_ID': object_ID,
+                                   'rotation': int(rotation.replace('r', '')),
+                                   'vision': 'intact'})
         intact_dicts.append(intact_dict)
 
     return percept_dicts, intact_dicts
 
 
-def checkconsec(integerlist,
-                assertlen=None):
+def checkconsec(integerlist):
     """
     Helper function.
     Check whether a list of integers contains consecutive numbers. Returns True or False.
@@ -228,24 +230,20 @@ def add_catches(stimlist,
 
 
 def loopthroughtrials(stim_sequence,
-                      exp_info,
+                      output_csv='testdata.csv',
                       response_key='space',
+                      escape_key='escape',
                       fullscreen=True,
                       windowsize=(1680, 1050),
                       stimsize=(400, 400),
                       fixdur=.500,
                       stimdur=1.,
-                      blankdur=.500,
-                      datapath='data'):
+                      blankdur=.500):
     """
     # TODO: docstring
     """
 
-    """
-    Initiate objects for window, fixation cross, stimulus, clocks, and trial handler.
-    """
-
-    # TODO: set window and stim size according to scanner hardware
+    # Initiate  window, fixation cross, stimulus, clocks, and trial handler.
     win = visual.Window(color='black', fullscr=fullscreen, units='pix')  # size=windowsize
     fixation = visual.ShapeStim(win, size=20,
                                 vertices=((0, -0.5), (0, 0.5), (0, 0), (-0.5, 0), (0.5, 0)),
@@ -254,37 +252,19 @@ def loopthroughtrials(stim_sequence,
                                 lineColor="white")
     blank = visual.ShapeStim(win, size=0, lineWidth=0, lineColor='black')  # blank is empty shape stim
     stim = visual.ImageStim(win, size=stimsize)
-    # contrast=1.0, opacity=1.0, depth=0
+    # trials = data.TrialHandler(stim_sequence, nReps=1, extraInfo=exp_info, method='sequential', originPath=datapath)
 
-    trials = data.TrialHandler(stim_sequence, nReps=1, extraInfo=exp_info,
-                               method='sequential', originPath=datapath)
-
+    # clocks
+    # TODO: logg time passed overall
     fix_rt = core.Clock()
     stim_rt = core.Clock()
     blank_rt = core.Clock()
 
-    """
-    loop through trials
-    """
-    for trial in trials:
-        win.flip()
-        print(trial)
-        print(os.path.exists(trial['file_path']))
+    # trial loop
+    for trial in stim_sequence:
 
-        # TODO: empty key press list at beginning of each trial
-        # retrieve if this is a catch trial
-        # TODO: don't create new variable to save computaitonal time
-        if trial['trial_type'] == 'catch':
-            catch = True
-        elif trial['trial_type'] == 'normal':
-            catch = False
-        else:
-            raise IOError('couldnt recognize trial_type : %s' % trial['trial_type'])
-
-        # initiate responses
-        acc = None
-        rt = None
-        escape_bool = False
+        # wait for keyboard input
+        keys = event.getKeys(keyList=[response_key, escape_key], timeStamped=stim_rt.getTime())
 
         # show fixation
         fix_rt.reset()
@@ -298,221 +278,73 @@ def loopthroughtrials(stim_sequence,
         while stim_rt.getTime() < stimdur:
             stim.draw()
             win.flip()
-            keys = event.waitKeys(maxWait=stimdur, keyList=[response_key, 'escape'],
-                                  timeStamped=stim_rt.getTime())
-            """
-            Evaluate response
-            """
-            # exit if escape key was pressed
-            if keys and 'escape' in keys[0]:
-                print('USER ESCAPED')
-                escape_bool = True
-            # no response
-            elif not keys:
-                if not catch:
-                    acc = 1
-                if catch:
-                    acc = 0
-            # response given
-            elif response_key in keys[0]:
-                rt = stim_rt.getTime()
-                if catch:
-                    acc = 1
-                if not catch:
-                    acc = 0
-            else:
-                print('RESPONSE EVALUATION FAILED')
-                escape_bool = True
 
-            # add data to TrialHandler
-            trials.addData('accuracy', acc)
-            trials.addData('RT', rt)
+        # response evaluation
+        trial['keys'] = keys
+        if keys:
+            if escape_key in keys[0]:
+                break
+            trial['RT'] = keys[0][1]
+            if trial['trial_type'] == 'normal':
+                trial['accuracy'] = 0
+            elif trial['trial_type'] == 'catch':
+                trial['accuracy'] = 1
+        else:
+            if trial['trial_type'] == 'normal':
+                trial['accuracy'] = 1
+            elif trial['trial_type'] == 'catch':
+                trial['accuracy'] = 0
 
         # TODO: show blank as jittered ITI
         blank_rt.reset()
         while blank_rt.getTime() < blankdur:
             blank.draw()
             win.flip()
+        # tag this trial as presented
+        trial['ran'] = True
 
-        if escape_bool:
-            break
+    # write output csv
+    header_keys = stim_sequence[0].keys()
+    with open(output_csv, 'wb') as f:
+        dict_writer = csv.DictWriter(f, header_keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(stim_sequence)
 
     # end presentation
-    # TODO: make output csv file name variable
-    trials.saveAsWideText('testdata.csv', delim=',')
     win.close()
+    core.quit()
 
     return None
 
 
-"""
-dummy code for testing
-"""
-perc_dir = '/Users/Oliver/ri_hmax/experiments/RI_objects_RSA/Stimuli/percepts'
-prep_dir = '/Users/Oliver/ri_hmax/experiments/RI_objects_RSA/Stimuli/preprocessed'
-
-# exp_info, outfile = create_gui()
-exp_info = {'Alter': ' 1', 'Gechlecht': 'maennlich', 'Rechtshaendig': True, 'Sitzung': 1, 'SubjectID': '1',
-            'date': u'2018_Nov_06_1408', 'exp_name': 'retina_rep'}
-percept_dicts, intact_dicts = get_stims_aloi_selection(percepts_dir=perc_dir, preprocessed_dir=prep_dir)
-stim_seq = add_catches(percept_dicts)
-
-win = visual.Window(color='black', fullscr=True, units='pix')  # size=windowsize
-stim = visual.ImageStim(win)
-stim.setImage('/Users/Oliver/ri_hmax/experiments/RI_objects_RSA/Stimuli/test.png')
-stim.draw()
-win.flip()
-core.wait(3)
-win.close()
-
-#loopthroughtrials(stim_seq, exp_info)
-
-
-def runseq_deprecated(stimlist,
-                      exp_info,
-                      session,
-                      datapath='data',
-                      fixdur=1.,
-                      stimdur=3.,
-                      maskdur=1.,
-                      fulls=True,
-                      stimsize=(600, 400)):
+if __name__ == '__main__':
     """
-    Procedure of stimulus presentation. Initiates everything necessary and then loops through the trials.
-
-    Parameters
-    ----------
-    stimlist : list of dicts
-        Specifies stimulus sequence.
-    exp_info : dict
-        contains general info about the experiment, such as date, subject ID, etc.
-    session : int
-        First or second session.
-    datapath : string
-        relative path to data directory for output csv files.
-    fixdur : float
-        duration of fixation cross.
-    stimdur : float
-        duration of stimulus presentation.
-    maskdur  float
-        duration of mask image presented after stimulus.
-    fulls : bool
-        fullscreen mode?
-    stimsize : tuple
-        resolution of stimulus in pixels
-
-    Returns
-    -------
-    None
+    dummy code for testing
     """
+    perc_dir = '/Users/Oliver/ri_hmax/experiments/RI_objects_RSA/Stimuli/percepts'
+    prep_dir = '/Users/Oliver/ri_hmax/experiments/RI_objects_RSA/Stimuli/preprocessed'
 
-    """
-    Initiate instances for window, fixation cross, and stimulus
-    """
+    # exp_info, outfile = create_gui()
+    exp_info = OrderedDict({'Alter': ' 1', 'Geschlecht': 'maennlich', 'Rechtshaendig': True, 'Sitzung': 1, 'SubjectID': '1',
+                'date': u'2018_Nov_06_1408', 'exp_name': 'retina_rep'})
 
-    win = visual.Window(size=(1680, 1050), color='black', fullscr=fulls, units='pix')
-    fixation = visual.ShapeStim(win, size=20,
-                                vertices=((0, -0.5), (0, 0.5), (0, 0), (-0.5, 0), (0.5, 0)),
-                                lineWidth=5,
-                                closeShape=False,
-                                lineColor="white")
-    stim = visual.ImageStim(win, size=stimsize)
-    # list of random mask stimuli, generate beforehand for speed.
-    maskstims = [create_maskstim(win, stimsize) for i in range(len(stimlist))]
+    percept_dicts, intact_dicts = get_stims_aloi_selection(percepts_dir=perc_dir, preprocessed_dir=prep_dir)
 
-    def _showtrial_deprecated(trial_instance, trialhandler, maskimg):
-        """
-        Sequence for one trial.
-        """
+    # TODO: function to add exp_info and response vars o stimulus dicts. Maybe include in add_catches.
+    for stimdicts in [percept_dicts, intact_dicts]:
+        for stimdict in stimdicts:
+            stimdict.update(exp_info)
 
-        # retrieve if this is a catch trial
-        catch = trial['catch']
-        assert type(catch) == bool
+    # initiate empty responses in each stimulus dict
+    for trial_num, stimdict in enumerate(percept_dicts):
+        stimdict['RT'] = None
+        stimdict['accuracy'] = None
+        stimdict['keys'] = None
+        stimdict['trial_num'] = trial_num
+        stimdict['ran'] = False
 
-        # initiate boolian to exit procedure with 'escape'
-        escape_bool = False
+    # add catches to sequence
+    stim_seq = add_catches(percept_dicts)
 
-        # initiate responses
-        acc = None
-        rt = None
-
-        # show fixation
-        fix_rt.reset()
-        while fix_rt.getTime() < fixdur:
-            fixation.draw()
-            win.flip()
-
-        # show stimulus
-        stim.setImage(trial_instance['imagepath'])
-        stim_rt.reset()
-        while stim_rt.getTime() < stimdur:
-            stim.draw()
-            win.flip()
-            keys = event.waitKeys(maxWait=stimdur, keyList=['space', 'escape'],
-                                  timeStamped=stim_rt.getTime())
-            """
-            Evaluate response
-            """
-            # TODO: RT and accuracy is not written in csv file anymore .. fix that!
-            # exit with escape key
-            if keys and 'escape' in keys[0]:
-                print('USER ESCAPED')
-                escape_bool = True
-            # no response
-            elif not keys:
-                if not catch:
-                    acc = 1
-                if catch:
-                    acc = 0
-            # response given
-            elif keys:
-                rt = stim_rt.getTime()
-                if catch:
-                    acc = 1
-                if not catch:
-                    acc = 0
-            else:
-                print('RESPONSE EVALUATION FAILED')
-                escape_bool = True
-
-            # add data to TrialHandler
-            trialhandler.addData('accuracy', acc)
-            trialhandler.addData('RT', rt)
-
-        # show mask
-        mask_rt.reset()
-        while mask_rt.getTime() < maskdur:
-            maskimg.draw()
-            win.flip()
-
-        return escape_bool
-
-    """
-    Loop through trials
-    """
-
-    # initiate timer for fixation cross and stimulus
-    fix_rt = core.Clock()
-    stim_rt = core.Clock()
-    mask_rt = core.Clock()
-
-    # initiate trial handler
-    trials = data.TrialHandler(stimlist, nReps=1, extraInfo=exp_info,
-                               method='sequential', originPath=datapath)
-
-    # loop over trials
-    for trial, mask in zip(trials, maskstims):
-        # show trial
-        escape_bool = _showtrial_deprecated(trial, trials, mask)
-        # exit if escape was pressed
-        if escape_bool:
-            break
-
-    # save csv
-    csv_outfile = pjoin(datapath,
-                        exp_info['SubjectID'] + '_' + exp_info['date'] + '_sitzung_' + str(session) + '.csv')
-    print('saving output file')
-    trials.saveAsWideText(csv_outfile, delim=',')
-
-    # close window
-    win.close()
+    # start stimulus presentation loop
+    loopthroughtrials(stim_seq, exp_info)
